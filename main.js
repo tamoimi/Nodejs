08faaee9303c5a0b4a3edf9de589776924dc0f13
tami <tamiortami@gmail.com>
2025-04-15 18:16:53 +0200

    error: delete method

--- main.js
+++ main.js
@@ -42,7 +42,7 @@ app.get("/page/:pageId", (request, response) => {
         `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
         ` <a href="/create">create</a>
                 <a href="/update/${sanitizedTitle}">update</a>
-                <form action="delete_process" method="post">
+                <form action="/delete_process" method="post">
                   <input type="hidden" name="id" value="${sanitizedTitle}">
                   <input type="submit" value="delete">
                 </form>`
@@ -136,9 +136,24 @@ app.post("/update_process", (request, response) => {
     var description = post.description;
     fs.rename(`data/${id}`, `data/${title}`, function (error) {
       fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-        response.writeHead(302, { Location: `/?id=${title}` });
-        response.end();
+        response.redirect(`/?id=${title}`);
       });
+    });
+  });
+});
+
+// delete form data
+app.post("/delete_process", (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var filteredId = path.parse(id).base;
+    fs.unlink(`data/${filteredId}`, function (error) {
+      response.redirect("/");
     });
   });
 });
