1485f9e87d53cec67133f20fe348688dd0d3f29d
tami <tamiortami@gmail.com>
2025-04-15 17:56:20 +0200

    refactor: to express

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 // =============================================================================
-// code with express 
+// code with express
 // =============================================================================
 // express = function, getting application
 const express = require("express");
 const app = express();
 const fs = require("fs");
 const template = require("./lib/template.js");
+var path = require("path");
+var sanitizeHtml = require("sanitize-html");
+const qs = require("querystring");
 
 // get("/") = route, res.send = what you wanna show through the route
 app.get("/", (request, response) =>
@@ -22,6 +25,123 @@ app.get("/", (request, response) =>
     response.send(html);
   })
 );
+
+app.get("/page/:pageId", (request, response) => {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(filelist);
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/create">create</a>
+                <a href="/update/${sanitizedTitle}">update</a>
+                <form action="delete_process" method="post">
+                  <input type="hidden" name="id" value="${sanitizedTitle}">
+                  <input type="submit" value="delete">
+                </form>`
+      );
+      response.send(html);
+    });
+  });
+});
+
+// create form
+app.get("/create", (request, response) => {
+  fs.readdir("./data", function (error, filelist) {
+    var title = "WEB - create";
+    var list = template.list(filelist);
+    var html = template.HTML(
+      title,
+      list,
+      `
+          <form action="/create_process" method="post">
+            <p><input type="text" name="title" placeholder="title"></p>
+            <p>
+              <textarea name="description" placeholder="description"></textarea>
+            </p>
+            <p>
+              <input type="submit">
+            </p>
+          </form>
+        `,
+      ""
+    );
+    response.send(html);
+  });
+});
+
+// post form data
+app.post("/create_process", (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.writeHead(302, { Location: `/?id=${title}` });
+      response.end();
+    });
+  });
+});
+
+// get form data
+app.get("/update/:pageId", (request, response) => {
+  fs.readdir("./data", function (error, filelist) {
+    var filteredId = path.parse(request.params.pageId).base;
+    fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+      var title = request.params.pageId;
+      var list = template.list(filelist);
+      var html = template.HTML(
+        title,
+        list,
+        `
+            <form action="/update_process" method="post">
+              <input type="hidden" name="id" value="${title}">
+              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+              <p>
+                <textarea name="description" placeholder="description">${description}</textarea>
+              </p>
+              <p>
+                <input type="submit">
+              </p>
+            </form>
+            `,
+        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+      );
+      response.send(html);
+    });
+  });
+});
+
+// update form data
+app.post("/update_process", (request, response) => {
+  var body = "";
+  request.on("data", function (data) {
+    body = body + data;
+  });
+  request.on("end", function () {
+    var post = qs.parse(body);
+    var id = post.id;
+    var title = post.title;
+    var description = post.description;
+    fs.rename(`data/${id}`, `data/${title}`, function (error) {
+      fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+        response.writeHead(302, { Location: `/?id=${title}` });
+        response.end();
+      });
+    });
+  });
+});
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
 
